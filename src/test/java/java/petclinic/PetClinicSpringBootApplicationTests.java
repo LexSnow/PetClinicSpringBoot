72750f787df8b65d03272e8e44feb6561bfc9493
kotoneyuu@gmail.com <Piosenka12!?>
2022-07-28 11:16:17 +0200

    Project creation

---
+++ src/test/java/java/petclinic/PetClinicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package java.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
